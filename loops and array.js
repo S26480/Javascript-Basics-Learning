62c96ad9d87a033003cf9a44b55796baa1269000
Sameeha A <sameeha1830@gmail.com>
2026-09-12 18:55:13 +0530

    Add loops and array methods practice

---
+++ loops and array.js
@@ -0,0 +1,53 @@
+// 1. Looping Through an Array - Create an array and print all values using four different loops
+
+//const numbers=[10,20,30,40];
+
+//for(let i=0; i<numbers.length; i++){
+//    console.log(numbers[i]);
+//}
+
+//numbers.forEach((num,i)=>{
+//    console.log(num)
+//})
+
+//for(let num of numbers){
+//    console.log(num);
+//}
+
+//for(let i in numbers){
+//    console.log(numbers[i]);
+//}
+
+
+//2. Loop Through an Object - Create an object and display all keys and values using a for...in loop.
+//const student={
+  //  name: "Bala",
+  //   age: 21,
+  // grade: "A"
+//};
+
+//for(let key in student){
+//   console.log(key,student[key]);
+//}
+
+
+//3.  Using map() - Create a new array where each number is reduced by 10.
+//const marks=[50,60,70,80];
+//const finalMarks=marks.map(num=>num-10);
+//console.log(finalMarks);
+
+
+//4. Using filter() - Return numbers greater than 10.
+//const values=[5,12,8,25,3,15];
+//const newValues=values.filter((num)=>{
+//    return num>10;
+//})
+//console.log(newValues);
+
+
+//5. Using reduce() - Find the total sum of all numbers in an array.
+//const nums = [5,10,15,20];
+//let sum=0;
+//const total=nums.reduce((sum,nums)=> sum+nums,0)
+//console.log(total);
+
